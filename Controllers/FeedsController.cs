572e7076ab1ff6646052d998f69f4ebe20b3fda9
manishp108 <manish.developer18@gmail.com>
2026-01-16 15:03:21 +0530

    Added FeedsController and Feed entity

---
+++ Controllers/FeedsController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Backend_trial_project.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FeedsController : ControllerBase
+    {
+    }
+}
---
+++ Entities/Feeds.cs
@@ -0,0 +1,9 @@
+﻿using System.Text.Json.Serialization;
+
+namespace BackEnd.Entities
+{
+    public class Feed
+    {
+
+        
+       }}
