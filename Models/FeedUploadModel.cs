9226c69a81411d70ba71af9da7eedf569f8d91f0
manishp108 <manish.developer18@gmail.com>
2026-01-22 17:06:48 +0530

    Added properties like File, UserId and FileName inside the model class- FeedUploadModel.cs

--- Models/FeedUploadModel.cs
+++ Models/FeedUploadModel.cs
@@ -5,7 +5,14 @@ namespace BackEnd.Models
 {
     public class FeedUploadModel
     {
+        [JsonPropertyName("file")]
+        public IFormFile File { get; set; } = null!; // Use IFormFile to handle file upload
 
-        
+        [JsonPropertyName("userId")]
+        public string UserId { get; set; } = string.Empty;
+
+        [JsonPropertyName("fileName")]
+        public string FileName { get; set; } = string.Empty; // User-selected file name
+
     }
 }
