adc5d10b1cd9a942d7dfd7d1ed52e20ea88c28bf
manishp108 <manish.developer18@gmail.com>
2026-01-23 18:14:59 +0530

    Add static ShortGuidGenerator utility for PostId generation

---
+++ ShortGuidGenerator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Text;
+
+public static class ShortGuidGenerator      // created static class ShortGuidGenerator
+{                                           // Has no state (no variables stored), Just performs one small utility task
+
+                                             //  Does not depend on object data
+    public static string Generate()
+    {
+        return Guid.NewGuid()
+                   .ToString("N")
+                   .Substring(0, 12);
+    }
+}
