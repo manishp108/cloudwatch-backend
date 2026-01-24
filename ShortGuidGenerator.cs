554724f152e02d641919ea4bb1a803f513641254
manishp108 <manish.developer18@gmail.com>
2026-01-24 11:49:25 +0530

    Added ShortGuidGenerator for generating random alphanumeric strings

--- ShortGuidGenerator.cs
+++ ShortGuidGenerator.cs
@@ -1,14 +1,24 @@
 ﻿using System;
 using System.Text;
 
-public static class ShortGuidGenerator      // created static class ShortGuidGenerator
-{                                           // Has no state (no variables stored), Just performs one small utility task
+public static class ShortGuidGenerator        // created static class ShortGuidGenerator
+{                                             // Has no state (no variables stored), Just performs one small utility task
+                                              //  Does not depend on object data
 
-                                             //  Does not depend on object data
-    public static string Generate()
+    private const string Characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+    public static string Generate(int length = 6)
     {
-        return Guid.NewGuid()
-                   .ToString("N")
-                   .Substring(0, 12);
+        if (length <= 0)
+            throw new ArgumentException("Length must be greater than zero.", nameof(length));
+
+        var stringBuilder = new StringBuilder(length);
+        var random = new Random();
+
+        for (int i = 0; i < length; i++)
+        {
+            stringBuilder.Append(Characters[random.Next(Characters.Length)]);
+        }
+        return stringBuilder.ToString();
     }
 }
