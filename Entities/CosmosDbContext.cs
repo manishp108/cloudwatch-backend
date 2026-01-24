12bd1e84b7626d8a1a5267a5c51a44d793ac5291
manishp108 <manish.developer18@gmail.com>
2026-01-24 16:48:20 +0530

    add PostsContainer configuration to CosmosDbContext

--- Entities/CosmosDbContext.cs
+++ Entities/CosmosDbContext.cs
@@ -1,6 +1,25 @@
-﻿namespace BackEnd.Entities
+﻿using Microsoft.Azure.Cosmos;
+
+namespace BackEnd.Entities
 {
     public class CosmosDbContext
     {
+
+        public Container PostsContainer { get; }  // Expose Cosmos DB container for posts collection
+
+
+        public CosmosDbContext(CosmosClient cosmosClient, IConfiguration configuration)
+        {
+            var databaseName = configuration["CosmosDbSettings:DatabaseName"];      // Read Cosmos DB database name from configuration
+
+            var postsContainerName = configuration["CosmosDbSettings:PostsContainerName"];
+
+
+
+
+            PostsContainer = cosmosClient.GetContainer(databaseName, postsContainerName);      // Initialize Cosmos DB container for posts
+
+
+        }
     }
-}
+    }
