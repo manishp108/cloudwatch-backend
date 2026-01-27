bb0bf6f59a758e8709bdfabff93f15a339a060ab
manishp108 <manish.developer18@gmail.com>
2026-01-27 10:51:41 +0530

    Created HomeController and implement default Index view action

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BackEnd.Controllers
+{
+    public class HomeController : Controller         // Controller responsible for handling home page requests
+    {
+        public IActionResult Index()           // Default action method that returns the Home/Index view
+        {
+            return View();
+        }
+    }
+}
