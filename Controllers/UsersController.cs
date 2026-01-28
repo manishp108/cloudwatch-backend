d6f58859801d6058a9038e7659e58f08d2979acd
manishp108 <manish.developer18@gmail.com>
2026-01-28 15:32:31 +0530

    Added UsersController with base API configuration

---
+++ Controllers/UsersController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+
+namespace BackEnd.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController : ControllerBase
+    {
+
+
+        public UsersController()
+        {
+            // Constructor reserved for future dependency injection
+            // (e.g., user services, database context, logger)
+
+
+        }
+
+
+
+
+    }
+}
