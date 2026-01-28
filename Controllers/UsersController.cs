82cb9de545465f17b3b6c040ac2dc803190ba717
manishp108 <manish.developer18@gmail.com>
2026-01-28 15:57:33 +0530

    -Added try-catch block. -Implemented profile picture update with Blob Storage upload and error handling. - Added utility static method to extract file name from URL inside the Shared folder

--- Controllers/UsersController.cs
+++ Controllers/UsersController.cs
@@ -1,6 +1,8 @@
 ﻿using Azure.Storage.Blobs;
 using BackEnd.Entities;
+using BackEnd.Models;
 using Microsoft.AspNetCore.Mvc;
+using BackEnd.Shared;
 
 
 namespace BackEnd.Controllers
@@ -12,6 +14,8 @@ namespace BackEnd.Controllers
 
         private readonly CosmosDbContext _dbContext;
         private readonly BlobServiceClient _blobServiceClient;
+        private readonly string _profileContainer = "profilepics";  // Blob container for storing profile pictures
+
         public UsersController(CosmosDbContext dbContext, BlobServiceClient blobServiceClient)
         {
             // Constructor reserved for future dependency injection
@@ -22,12 +26,34 @@ namespace BackEnd.Controllers
         }
 
         [HttpPost("updateUser")]
-        public IActionResult UpdateUser()
+        public async Task<IActionResult> UpdateUser([FromForm] FeedUploadModel model)  // Async method used to handle non-blocking I/O operations (DB and file uploads)
+
         {
-            return Ok();
+            try
+            {
+                string updatedPicURL = string.Empty;
+
+                if (model.File != null && !string.IsNullOrEmpty(model.UserId) && !string.IsNullOrEmpty(model.FileName) && !string.IsNullOrEmpty(model.ProfilePic))
+                {
+                    var containerClient = _blobServiceClient.GetBlobContainerClient(_profileContainer);
+                    var blobName = Utility.GetFileNameFromUrl(model.ProfilePic);
+                    var blobClient = containerClient.GetBlobClient(blobName);
+
+                    using (var stream = model.File.OpenReadStream())
+                    {
+                        await blobClient.UploadAsync(stream, overwrite: true);
+                    }
+
+                    updatedPicURL = blobClient.Uri.ToString(); // Direct Blob Storage URL
+                }
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"Error updating profile picture: {ex.Message}");
+            }
+
         }
-
-
 
     }
 }
---
+++ Shared/Utility.cs
@@ -0,0 +1,18 @@
+﻿namespace BackEnd.Shared
+{
+    public static class Utility
+    {
+        public static string GetFileNameFromUrl(string url)          // Extracts the file name from a given URL
+        {
+            // Create a Uri object from the URL
+            Uri uri = new Uri(url);
+
+            // Get the last part of the path (file name)
+            string fileName = System.IO.Path.GetFileName(uri.LocalPath);
+
+            return fileName;
+        }
+
+        
+    }
+}
