2e4bf7aaaccb07f7e0f766d5afa518f7a1071de6
manishp108 <manish.developer18@gmail.com>
2026-01-28 17:45:48 +0530

    -Added UserPostController inside Controller folder

---
+++ Controllers/UserPostController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+
+namespace BackEnd.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UserPostController : ControllerBase
+    {
+        
+        public UserPostController()
+        {
+            
+        }
+
+    }
+}
