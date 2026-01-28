15685374e703871dd6f1655639f30275164327a5
manishp108 <manish.developer18@gmail.com>
2026-01-28 09:09:36 +0530

    Added MessagesController with base API configuration

---
+++ Controllers/MessagesController.cs
@@ -0,0 +1,22 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+
+namespace BackEnd.Controllers
+{
+    [ApiController]   // Marks this class as a Web API controller
+    [Route("api/[controller]")]
+    public class MessagesController : ControllerBase
+    {
+
+        public MessagesController()
+        {
+            // Constructor reserved for future dependency injection
+            // (e.g., services, database context, logger, etc.)
+        }
+
+
+
+
+
+    }
+}
