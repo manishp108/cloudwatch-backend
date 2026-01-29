44859c52d159a4472ee0a653cedabd0a68a4c05f
manishp108 <manish.developer18@gmail.com>
2026-01-29 11:08:44 +0530

    Added BlogPostEditViewModel class inside ViewModels and purpose of this is used to shape and protect data

---
+++ ViewModels/BlogPostEditViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BackEnd.ViewModels
+{
+    public class BlogPostEditViewModel
+    {
+        public string PostId { get; set; } // Unique identifier of the blog post
+
+
+        [Required(AllowEmptyStrings = false)]  // Blog post title (required)
+        public string Title { get; set; }
+
+
+        [Required(AllowEmptyStrings = false)] // Blog post content (required)
+        public string Content { get; set; }
+
+    }
+}
